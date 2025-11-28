8f57df2bc170c25ff2fefdefdd3ed33dc240b990
deeps-19 <deepalimore609@gmail.com>
2025-11-28 11:35:16 +0530

    added main server

--- Front/tasktracker/src/api/axios.js
+++ Front/tasktracker/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://employee-tracker-du2a.onrender.com/api",
 });
 
 // Add token automatically
